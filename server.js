8755250bb64dcdb68fa2e535ffef22737ff71f4b
AgesilausDrako <agesilaus@yahoo.com>
2018-01-12 17:43:39 -0500

    refactored initialData

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(sassMiddleware({
 app.set('view engine', 'ejs');
 
 import serverRender from './serverRender';
-app.get('/', (req, res) => {
-    serverRender()
+app.get(['/', '/contest/:contestId'], (req, res) => {
+    serverRender(req.params.contestId)
         .then(( { initialMarkup, initialData }) => {
             res.render('index', {
                 initialMarkup,
